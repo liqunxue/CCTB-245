2e5d0b3b656f3c91070669bb9d537ed856cffe26
skrcan <skrcan@hotmail.com>
2014-05-28 10:38:18 -0600

    Collection- Array/List

--- Chapter 6 - Alt/CardGame/Program.cs
+++ Chapter 6 - Alt/CardGame/Program.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿// using statements allow me access to the classes in the stated namespaces
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CardGame.AdHoc_Demo; // To have access to my various collection-class demos
 
 namespace CardGame
 {
@@ -10,6 +12,13 @@ namespace CardGame
     {
         static void Main(string[] args)
         {
+            // Run some of the demos
+            // ArrayDemo demo1 = new ArrayDemo();
+            // demo1.Run();
+
+            ListDemo demo2 = new ListDemo();
+            demo2.Run();
+
         }
     }
 }
